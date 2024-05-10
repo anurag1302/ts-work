b1719e2d50ef4b40d008add86522c417efbf9893
Anurag <anurag3487@gmail.com>
2024-05-10 15:46:53 +0530

    ts functions

--- functions.js
+++ functions.js
@@ -9,3 +9,21 @@ const multiply = function (num1, num2) {
 console.log(add(4, 3));
 console.log(subtract(4, 3));
 console.log(multiply(4, 3));
+//Optional Params
+function add1(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
+}
+//Required Params/Default Params
+const subtract1 = (num1, num2, num3 = 10) => num1 - num2 - num3;
+//Rest params
+const multiply1 = function (num1, num2, num3) {
+    return num1 * num2 * num3.reduce((a, b) => a * b);
+};
+const multiply2 = function (num1, num2, ...num3) {
+    return num1 * num2 * num3.reduce((a, b) => a * b, 1);
+};
+console.log(add1(4, 3, 5));
+console.log(subtract1(4, 3, 9));
+console.log(multiply1(4, 3, [1, 2, 3]));
+console.log(multiply2(4, 3, ...[1, 2, 3]));
+console.log(multiply2(4, 3, 1, 2, 3));
--- functions.ts
+++ functions.ts
@@ -8,6 +8,29 @@ const multiply = function (num1: number, num2: number): number {
     return num1 * num2;
 }
 
-console.log(add(4,3))
-console.log(subtract(4,3))
-console.log(multiply(4,3))
+console.log(add(4, 3))
+console.log(subtract(4, 3))
+console.log(multiply(4, 3))
+
+//Optional Params
+function add1(num1: number, num2: number, num3?: number): number {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
+}
+
+//Required Params/Default Params
+const subtract1 = (num1: number, num2: number, num3 = 10): number => num1 - num2 - num3;
+
+//Rest params
+const multiply1 = function (num1: number, num2: number, num3: number[]): number {
+    return num1 * num2 * num3.reduce((a, b) => a * b);
+}
+
+const multiply2 = function (num1: number, num2: number, ...num3: number[]): number {
+    return num1 * num2 * num3.reduce((a, b) => a * b, 1);
+}
+
+console.log(add1(4, 3, 5))
+console.log(subtract1(4, 3, 9))
+console.log(multiply1(4, 3, [1, 2, 3]))
+console.log(multiply2(4, 3, ...[1, 2, 3]))
+console.log(multiply2(4, 3, 1, 2, 3))
